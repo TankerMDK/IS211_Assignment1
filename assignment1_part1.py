afa5a97f65174db5e76b3c185250aa38fc488c99
TankerMDK <TankerMDK@Gmail.com>
2025-02-02 21:24:16 -0500

    made syntax corrections and added code.

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,19 +1,28 @@
-
-def list_divide(numbers, divide):
+def list_divide(numbers, divide=2):
     """
-    The function returns the number of elements in the numbers list that are divisibleby divide
+    The function returns the number of elements in the numbers list that are divisible by divide
+    """
+    count = 0
+    for number in numbers:
+        if number % divide == 0:
+            count += 1
+    return count
+
+class ListDivideException(Exception):
+    """
+    This is the requested custom exception for the list_divide function
     """
     pass
 
 def test_list_divide():
     """
-    Test listDivide
+    Test listDivide | [Below, renamed the function to match the list_divide function] -RA
     """
-    assert listDivide([1,2,3,4,5]) == 2
-    assert listDivide([2,4,6,8,10]) == 5
-    assert listDivide([30, 54, 63,98, 100], divide=10) == 2
-    assert listDivide([]) == 0
-    assert listDivide([1,2,3,4,5], 1) == 5
+    assert list_divide([1,2,3,4,5]) == 2
+    assert list_divide([2,4,6,8,10]) == 5
+    assert list_divide([30, 54, 63,98, 100], divide=10) == 2
+    assert list_divide([]) == 0
+    assert list_divide([1,2,3,4,5], 1) == 5
     
 if __name__ == "__main__":
-    testListDivide()
+    test_list_divide()
