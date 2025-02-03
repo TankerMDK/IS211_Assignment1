07cf136fdfaca31805aac97864f2cfcc663b0bda
TankerMDK <TankerMDK@Gmail.com>
2025-02-02 23:49:36 -0500

    I've added as many notes as I could. I'm practicing this because it's going to be a key point for the projects.

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,12 +1,29 @@
-
 class Book:
-    def __init__(self, author, title):
-        pass
-    
-    def display(self):
-        pass
+    def __init__(self, author="", title=""):
+        self.author = author
+        self.title = title
 
+    """
+    Step 2b: added the instance attribute, author and title for the object.
+    """
+
+    def display(self):
+        print(f" {self.title}, written by {self.author}")
+
+    """
+    Step 2c: the display function prints the title and author of the book.
+    """
+
+
+book1 = Book("J. K. Rowling", "Harry Potter and the Goblet of Fire")
+book2 = Book("Walter Scott", "Of Mice and Men")
+"""
+Step 3: instatiating the book objects.
+"""
 
 if __name__ == "__main__":
-    a = Book("Thorpe", "Beat the Dealer")
-    print("This is a assignment 1 part 2")
+    book1.display()
+    book2.display()
+    """
+    Step 4: calling the display function on the book objects.
+    """
